af41ab5092532174df358518214236c8e8180c48
duartegavilan <duartegavilan@gmail.com>
2016-01-04 20:35:04 +0000

    config

---
+++ tentativa.php
@@ -0,0 +1,20 @@
+<?php
+
+require("reddit.php");
+$username = "duartegavilan";
+$password = "Dudinhah1991";
+
+$reddit = new reddit($username, $password);
+
+
+?>
+
+<html> 
+<head>
+<title>Reddit PHP</title>
+</head>
+<body>
+
+
+</body>
+</html>
